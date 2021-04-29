6fd45eac6d92a7a1cbc2c545f874bcc6e8340e4d
Depem0n <depem0n@yahoo.com>
2021-04-30 00:47:43 +0300

    Initial commit

---
+++ rasa/actions/actions.py
@@ -0,0 +1,24 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionSlotsToJSON(Action):
+
+    def name(self) -> Text:
+        return "action_return_booking_info_json"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        entities = tracker.latest_message['entities']
+        output_dict = {e['entity']: e['value'] for e in entities}
+        dispatcher.utter_message(template="utter_booking_info",
+                                 number=output_dict['number_of_users'],
+                                 arrival_city=output_dict['arrival_city'],
+                                 date=output_dict['departure_date'],
+                                 time=output_dict['departure_time'],
+                                 departure_city=['departure_city'])
+
+        return []
